420ee0168f2ef04ddb23941f240769b9e2fbcd7d
manhhungcb <manhhungcb92@gmail.com>
2026-03-11 15:20:35 +0700

    add login api

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,7 +3,7 @@ import asyncHandler from '../middlewares/asyncHandler.js';
 import bcrypt from 'bcryptjs';
 import createToken from '../utils/createToken.js'
 
-const createUser = asyncHandler(async (req, res) => {
+const createUser = asyncHandler(async(req, res) => {
     const { username, email, password } = req.body;
     
     if (!username || !email || !password) {
@@ -33,4 +33,26 @@ const createUser = asyncHandler(async (req, res) => {
     }
 });
 
-export { createUser };
+const loginUser = asyncHandler(async(req, res) => {
+    const {email, password} = req.body
+
+    const existingUser = await User.findOne({email})
+
+    if (existingUser) {
+        const isPasswordValid = await bcrypt.compare(password, existingUser.password);
+
+        if (isPasswordValid) {
+            createToken(res, existingUser._id)
+
+            res.status(201).json({
+            _id: existingUser._id, 
+            username: existingUser.username, 
+            email: existingUser.email,
+            isAdmin: existingUser.isAdmin,     
+            });
+            return;
+        }
+    }
+})
+
+export { createUser, loginUser };
