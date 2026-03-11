3cef68a0b46fa7684f9943c97133eedf8bd5bf02
manhhungcb <manhhungcb92@gmail.com>
2026-03-11 15:08:50 +0700

    add jwt token and cookie

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,7 @@
 import User from '../models/userModel.js';
 import asyncHandler from '../middlewares/asyncHandler.js';
 import bcrypt from 'bcryptjs';
+import createToken from '../utils/createToken.js'
 
 const createUser = asyncHandler(async (req, res) => {
     const { username, email, password } = req.body;
@@ -15,16 +16,17 @@ const createUser = asyncHandler(async (req, res) => {
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
     const newUser = new User({username, email, password: hashedPassword})
-
-    res.status(201).json({
-        _id: newUser._id, 
-        username: newUser.username, 
-        email: newUser.email,
-        isAdmin: newUser.isAdmin,     
-    })
-
+    
     try {
-        await newUser.save()
+        await newUser.save();
+        createToken(res, newUser._id);
+        
+        res.status(201).json({
+            _id: newUser._id, 
+            username: newUser.username, 
+            email: newUser.email,
+            isAdmin: newUser.isAdmin,     
+        });
     } catch (error) {
         res.status(400)
         throw new Error("Thông tin không hợp lệ")
